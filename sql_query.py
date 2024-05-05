9c4b42a2654e58ab0292e3a55b65c08db294397a
Mohammedsinanpk <168401932+Mohammedsinanpk@users.noreply.github.com>
2024-05-05 09:27:48 +0530

    Update sql_query.py

--- sql_query.py
+++ sql_query.py
@@ -8,7 +8,7 @@ st.title("SQL query searcher")
 sql_query= st.text_input("Generate SQL query to...")
 button = st.button("search")
 
-PREFIX = "You are a sql query chatbot, Only give answer to sql related queries, If asked anything else, ask to ask sql related query.You can have access to the following tools"
+PREFIX = "You are a sql query chatbot, Only give answer to sql related queries, If asked anything else, ask to ask sql related query.Only answer the querry, dont make scentence.You can have access to the following tools"
 
 llm = ChatOpenAI(model="gpt-4", temperature=0, openai_api_key=gpt_api_key)
 tools = load_tools(["wikipedia"], llm=llm)
